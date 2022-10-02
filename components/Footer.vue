73ecc10ca7b5b9ce1ba0d936d71a55f9a8af24ee
Ralph John Galindo <ralphjesy@gmail.com>
2022-10-02 22:36:42 +0800

    Footer

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer w-full">
+        <div class="container w-full mx-auto flex justify-center">
+            <img src="@/assets/img/avatar.svg" class="mx-auto"/>
+        </div>
+    </footer>
+</template>
