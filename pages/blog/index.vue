437685f8a829989b8ea946db77c70f1be4ff62a9
Ralph John Galindo <ralphjesy@gmail.com>
2022-10-13 19:43:42 +0800

    Articles

---
+++ pages/blog/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="post-wrapper">
+        <Suspense>
+            <StoryblokComponent v-if="story" :story="story" :blok="story.content"/>
+            <template #fallback>
+                Loading...
+            </template>
+        </Suspense>
+    </div>
+</template>
+<style src="~/assets/scss/layouts/posts.scss" lang="scss">
+
+</style>
+<script setup>
+const config = useRuntimeConfig()
+
+const story = await useStoryblok('blog',
+    {
+        version: config.public.STORY_VERSION,
+    }
+)
+
+</script>
+       
