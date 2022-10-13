033a43c57c12d3ece8c67350315f317ce1d9ea95
Ralph John Galindo <ralphjesy@gmail.com>
2022-10-13 20:04:44 +0800

    Articles

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -23,5 +23,39 @@ const story = await useStoryblok('/blog/' + slug,
         version: config.public.STORY_VERSION,
     }
 )
+
+const route = useRoute()
+
+const info = {
+    title: `${story.value.content.title || 'Senior Web Developer'} - Ralph John` ,
+    description: (story.value.content.description || '').substr(0,165),
+    image: story.value.content.image.filename,
+    url: route.fullPath
+};
+
+useHead({
+    title: info.title,
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    htmlAttrs: { lang: 'en-PH' },
+    meta: [
+        { name: 'title', content: info.title },
+        { name: 'description', content: info.description },
+        { name: 'keywords', content: 'web, portfolio, frontend, developer, jamstack, philippines, ' + (story.value.tag_list || []).join(', ') },
+
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: info.url },
+        { property: "og:title", content: info.title },
+        { property: "og:description", content: info.description },
+        { property: "og:image", content: info.image },
+
+        { property: "twitter:card", content: "summary_large_image" },
+        { property: "twitter:url", content: info.url },
+        { property: "twitter:title", content: info.title },
+        { property: "twitter:description", content: info.description },
+        { property: "twitter:image", content: info.image }
+
+    ],
+})
 </script>
        
