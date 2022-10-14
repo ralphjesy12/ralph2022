d8517a1e8d257ce360e72013829df1ad9b398551
Ralph John Galindo <ralph@burstsms.com>
2022-10-14 21:50:52 +0800

    Icons on new tab

--- storyblok/Icon.vue
+++ storyblok/Icon.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hero-social-link" v-editable="blok">
-        <a :href="blok.link.url">
+        <a :href="blok.link.url" target="_blank">
             <img :src="(blok.icon.filename + '/m/')" class="hero-social-icon" :alt="blok.name">
         </a>
     </div>
