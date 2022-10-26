6c28eb63106d9f7ef8aaec6f1274f1ea084e0c3c
Ralph John Galindo <ralphjesy@gmail.com>
2022-10-26 10:27:29 +0800

    Add scroll events

---
+++ plugins/scrollEvents.js
@@ -0,0 +1,58 @@
+import { defineNuxtPlugin } from "#app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.$router.options.scrollBehavior = async (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    
+      
+
+    const findEl = async (hash, x = 0) => {
+      return (
+        document.querySelector(hash) ||
+        new Promise((resolve) => {
+          if (x > 0) {
+            return resolve(document.querySelector("#app"));
+          }
+          setTimeout(() => {
+            resolve(findEl(hash, 1));
+          }, 300);
+        })
+      );
+    };
+
+    document
+      .querySelectorAll('a[href*="#"]').forEach(function(el){
+        el.addEventListener("click", function (event) {
+          
+          const elHrefHash = '#' + (el.getAttribute('href')).split('#')[1];
+          const hashElement = document.querySelector(elHrefHash);
+          
+          if(hashElement){
+            event.preventDefault();
+
+            if ("scrollBehavior" in document.documentElement.style) {
+              return window.scrollTo({ top: hashElement.offsetTop, behavior: "smooth" });
+            } else {
+              return window.scrollTo(0, hashElement.offsetTop);
+            }
+          }
+
+          return true;
+          
+        });
+      })
+
+    if (to.hash) {
+      const el = await findEl(to.hash);
+      if ("scrollBehavior" in document.documentElement.style) {
+        return window.scrollTo({ top: el.offsetTop, behavior: "smooth" });
+      } else {
+        return window.scrollTo(0, el.offsetTop);
+      }
+    }
+    return { left: 0, top: 0, behaviour: "smooth" };
+  };
+});
